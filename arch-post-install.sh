f88dba81a78185373cb9232aaf9ae82e8fad704f
gxbrriellll <n.abreu.tejada@proton.me>
2025-09-16 11:06:05 -0400

    Update arch-post-install.sh

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
 
-sudo pacman -Syu --noconfirm lib32-mesa gdm gnome hyprland htop curl wget blueman pipewire alsa-utils pipewire-alsa pipewire-pulse
+sudo pacman -Syu --noconfirm lib32-mesa gdm gnome hyprland blueman alsa-utils pipewire-alsa pipewire-pulse
 pipewire-jack wireplumber pavucontrol noto-fonts noto-fonts-emoji ttf-font-awesome ttf-liberation ttf-dejavu rofi-emoji libxcb
 gnome-backgrounds p7zip xz gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg flatpak slurp
 inxi wayland-protocols xorg-xwayland waybar kitty qt5-graphicaleffects gnome-software power-profiles-daemon xorg swww rofi wayland
@@ -44,5 +44,6 @@ rm -rf ~/arch-dotfiles
 sudo systemctl enable gdm
 
 sudo systemctl start gdm
+
 
 
