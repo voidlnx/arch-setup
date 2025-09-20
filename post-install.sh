3319cae1bdda349096e967f33b4cdb4aa9cc636c
gxbrriellll <n.abreu.tejada@proton.me>
2025-09-20 17:35:57 +0000

    Update and rename apps-post-install.sh to post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -Syu --noconfirm lib32-mesa plasma lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings sassc hyprland blueman alsa-utils pipewire-alsa pipewire-pulse pipewire-jack wireplumber pavucontrol noto-fonts noto-fonts-emoji ttf-font-awesome ttf-liberation ttf-dejavu rofi-emoji libxcb p7zip xz gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg slurp inxi wayland-protocols xorg-xwayland waybar kitty qt5-graphicaleffects gnome-software power-profiles-daemon xorg swww rofi wayland qt5-wayland qt5-base qt5-xcb-private-headers kio kconfig kcoreaddons xdg-desktop-portal xdg-desktop-portal-wlr grim nautilus eog gnome-text-editor vlc-plugin-gstreamer vlc-plugin-ffmpeg vlc ntfs-3g pacman-contrib mesa mesa-utils mesa-demos libva-utils archlinux-keyring
+
+yay -Syu --noconfirm appimagelauncher waypaper cliphist wlogout
+
+flatpak install -y flathub com.github.tchx84.Flatseal io.gitlab.librewolf-community
