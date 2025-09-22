2963031249349e04e211b48db9b04be2446c8cd2
gxbrriellll <n.abreu.tejada@proton.me>
2025-09-22 16:55:15 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir -p ~/Pictures/Screenshots
+
+mkdir -p ~/.icons
+
+mkdir -p ~/.themes
+
+mkdir -p ~/.local/bin
+
 sudo pacman -Syu --noconfirm lib32-mesa plasma discord obs-studio qbittorrent steam kid3 keepassxc gnome-text-editor nautilus ark audacity sddm-kcm sassc hyprland firefox blueman alsa-utils pipewire-alsa pipewire-pulse pipewire-jack wireplumber pavucontrol noto-fonts noto-fonts-emoji ttf-font-awesome ttf-liberation ttf-dejavu rofi-emoji libxcb gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg slurp inxi wayland-protocols xorg-xwayland waybar kitty qt5-graphicaleffects power-profiles-daemon xorg swww rofi wayland qt5-wayland qt5-base qt5-xcb-private-headers kio kconfig kcoreaddons okular xdg-desktop-portal xdg-desktop-portal-wlr grim eog vlc-plugin-gstreamer vlc-plugin-ffmpeg vlc ntfs-3g pacman-contrib mesa mesa-utils mesa-demos libva-utils archlinux-keyring
 
 yay -Syu --noconfirm appimagelauncher waypaper cliphist wlogout youtube-music-bin xclicker heroic-games-launcher-bin
