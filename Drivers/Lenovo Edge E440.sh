04d7280b795b7670f7dca0c19fa9300482fcaa81
voidlnx <n.abreu.tejada@gmail.com>
2025-10-20 18:55:58 -0400

    Change package installation to include xf86-video-intel

--- Drivers/Lenovo Edge E440.sh
+++ Drivers/Lenovo Edge E440.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm intel-ucode vulkan-intel lib32-vulkan-intel
+sudo pacman -Syu --noconfirm xf86-video-intel vulkan-intel lib32-vulkan-intel
 
 sudo reboot now
