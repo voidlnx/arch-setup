ddec81823c3bd14927c8107c4e8353c188eca44c
voidlnx <n.abreu.tejada@gmail.com>
2025-10-22 21:41:45 -0400

    Update apps-install.sh

--- apps-install.sh
+++ apps-install.sh
@@ -2,7 +2,7 @@ sudo pacman -Syu firefox discord spotify-launcher audacity kid3 steam
 
 yay -Syu heroic-games-launcher-bin
 
-flatpak install -y flathub org.vinegarhq.Sober com.stremio.Stremio
+flatpak install -y flathub com.stremio.Stremio org.vinegarhq.Sober com.protonvpn.www
 
 flatpak override --user \
   --filesystem=xdg-run/app/com.discordapp.Discord:create \
