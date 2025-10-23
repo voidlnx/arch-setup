c73bcede266a5e0a94bed2412353b0f1f5182dba
voidlnx <n.abreu.tejada@gmail.com>
2025-10-22 21:36:50 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,3 +3,5 @@
 sudo pacman -Syu --noconfirm lib32-mesa xorg xorg-server xorg-xinit plasma sddm sddm-kcm gnome-text-editor nautilus ark sassc hyprland blueman alsa-utils pipewire-alsa pipewire-pulse pipewire-jack wireplumber pavucontrol noto-fonts noto-fonts-emoji ttf-font-awesome ttf-liberation ttf-dejavu rofi-emoji libxcb gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg slurp inxi wayland-protocols xorg-xwayland waybar kitty qt5-graphicaleffects power-profiles-daemon swww rofi wayland qt5-wayland qt5-base qt5-xcb-private-headers kio kconfig kcoreaddons xdg-desktop-portal xdg-desktop-portal-wlr grim eog vlc-plugin-gstreamer vlc-plugin-ffmpeg vlc ntfs-3g pacman-contrib mesa mesa-utils mesa-demos libva-utils
 
 yay -Syu --noconfirm appimagelauncher waypaper cliphist wlogout
+
+flatpak install -y flathub com.github.tchx84.Flatseal
